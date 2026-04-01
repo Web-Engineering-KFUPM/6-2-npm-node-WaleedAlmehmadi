7cc2b6b9ec7cb9e8e9be121ce11319dd2887e00e
cn7n <alme7madi14@gmail.com>
2026-04-01 08:31:14 +0300

    Last Commit

--- utils/operations.js
+++ utils/operations.js
@@ -1,15 +1,15 @@
 export function add(numbers) {
-  return {};
+  return numbers.reduce((sum, num) => sum + num, 0);
 }
 
 export function subtract(numbers) {
-  return {};
+  return numbers.slice(1).reduce((result, num) => result - num, numbers[0]);
 }
 
 export function multiply(numbers) {
-  return {};
+  return numbers.reduce((result, num) => result * num, 1);
 }
 
 export function divide(numbers) {
-  return {};
+  return numbers.slice(1).reduce((result, num) => result / num, numbers[0]);
 }
